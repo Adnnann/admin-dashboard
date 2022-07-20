47c2402eb71fc04da050402d283f46f0b5215d54
Adnnann <adnanovcina1987@gmail.com>
2022-07-20 21:45:46 +0200

    revised

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import passport from "passport";
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/user.routes.js";
 import userImageRoutes from "./routes/userImage.routes.js";
+import errorLogger from "./controllers/helpers/errorLogger.js";
 
 const app = express();
 
@@ -23,14 +24,17 @@ app.use("/", userRoutes);
 app.use("/images", express.static("images"));
 app.use("/", userImageRoutes);
 
-app.get("*", function (req, res) {
-  res.send(path.join(__dirname, "/public/index.html"));
+app.all("*", (req, res) => {
+  return res.status(404).send(`<h1>Unknown route</h1>`);
 });
 
 app.use((err, req, res, next) => {
   if (err.name === "UnauthorizedError") {
     res.status(401).json({ error: `${err.name} : ${err.message}` });
   }
+  errorLogger.error(`Date: ${new Date().toUTCString()} | Error: ${err.message}
+  | Type: ${err.name} | Stack: ${err.stack.trim()}`);
+  res.end("Server Error");
 });
 
 export default app;
